7c11175031ff4a092f70e5bd6fd670b4687c69bc
hyuk0309 <bahngeh@naver.com>
2023-06-28 17:34:13 +0900

    doc : init project

---
+++ src/main/kotlin/kr/elvis/playgroundspringdataredispubsub/PlaygroundSpringDataRedisPubsubApplication.kt
@@ -0,0 +1,11 @@
+package kr.elvis.playgroundspringdataredispubsub
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlaygroundSpringDataRedisPubsubApplication
+
+fun main(args: Array<String>) {
+    runApplication<PlaygroundSpringDataRedisPubsubApplication>(*args)
+}
---
+++ src/test/kotlin/kr/elvis/playgroundspringdataredispubsub/PlaygroundSpringDataRedisPubsubApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.elvis.playgroundspringdataredispubsub
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PlaygroundSpringDataRedisPubsubApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
